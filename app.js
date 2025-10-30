556f1340ae48eadb192d97044c93610ef226a12e
rutvijmavani <rutvijmavani@gmail.com>
2025-10-30 19:17:54 -0400

    added routes

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 import express from 'express'
+import { PORT } from './config/env.js'
+
+import userRouter from './routes/user.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+import authRouter from './routes/auth.routes.js'
+
 
 const app = express()
+
+app.use('/api/v1/auth' , authRouter)
+app.use('/api/v1/users' , userRouter)
+app.use('/api/v1/subscriptions' , subscriptionRouter)
 
 app.get('/' , (req , res) => {
     res.send('Welcome to the Subscription Management API!')
 })
 
-app.listen(3000 , () => {
-    console.log('Subscription Management API is running on http://localhost:3000')
+app.listen(PORT , () => {
+    console.log(`Subscription Management API is running on http://localhost:${PORT}`)
 })
 
 export default app
